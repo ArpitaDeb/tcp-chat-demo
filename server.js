775aa276cb46cc8c518d11025ee7ab949f5df796
Martin Laws <martinblaws@gmail.com>
2019-09-25 12:00:09 -0400

    Updated code formatting

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 // Load the TCP Library
 net = require('net')
 
-// Keep track of the chat clients
 const clients = []
 
 // Start a TCP Server
 net.createServer(socket => {
+  // Send a message to all clients
+  const broadcast = (message, sender) => {
+    clients.forEach(client => {
+      // Don't want to send it to sender
+      if (client === sender) return
+      client.write(message)
+    })
+    // Log it to the server output too
+    console.log(message)
+  }
+  
   socket.write(`Hello! What's your name?\n`)
 
   // Handle incoming messages from clients.
@@ -30,23 +40,11 @@ net.createServer(socket => {
     }
   })
 
-  // Remove the client from the list when it leaves
-  socket.on('end', function () {
+   // Remove the client from the list when it leaves
+   socket.on('end', function () {
     clients.splice(clients.indexOf(socket), 1)
-    broadcast(socket.name + " left the chat.\n")
+    // broadcast(socket.name + " left the chat.\n")
   })
-  
-  // Send a message to all clients
-  function broadcast(message, sender) {
-    clients.forEach(function (client) {
-      // Don't want to send it to sender
-      if (client === sender) return
-      client.write(message)
-    })
-    // Log it to the server output too
-    console.log(message)
-  }
-
 }).listen(1234)
 
 // Put a friendly message on the terminal of the server.
