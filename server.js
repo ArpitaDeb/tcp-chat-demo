899888b76c6ed381c48952309d8b5d469661ca2b
Martin Laws <martinblaws@gmail.com>
2019-12-04 12:06:28 -0500

    Updates from 12/04 lecture

--- server.js
+++ server.js
@@ -1,51 +1,49 @@
-// Load the TCP Library
 net = require('net')
 
 const clients = []
 
+const broadcast = (message, sender) => {
+  clients.forEach(client => {
+    // Don't want to send it to sender
+    if (client === sender) return
+    client.write(message)
+  })
+  // Log it to the server output too
+  console.log(message)
+}
+
 // Start a TCP Server
 net.createServer(socket => {
-  // Send a message to all clients
-  const broadcast = (message, sender) => {
-    clients.forEach(client => {
-      // Don't want to send it to sender
-      if (client === sender) return
-      client.write(message)
-    })
-    // Log it to the server output too
-    console.log(message)
-  }
-  
-  socket.write(`Hello! What's your name?\n`)
+  socket.write(`Hello! Welcome to the chatroom! Please enter your username: \n`)
 
-  // Handle incoming messages from clients.
-  socket.on('data', function (data) {
+  socket.on('data', message => {
     if (socket.name) {
-      broadcast(`${socket.name} > ${data}`, socket)
+      broadcast(`${socket.name} > ${message}`, socket)
     } else {
       // Identify this client
-      socket.name = String(data).trim()
+      socket.name = String(message).trim()
 
       // Put this new client in the list
       clients.push(socket)
 
       // Send a nice welcome message and announce
       socket.write(`
-        Welcome, ${socket.name}! You're person #${clients.length} in this chat.
+        Welcome ${socket.name}! You're person #${clients.length} in this chat.
         
         To send a message, type it out and press enter.
-      \n`)
+      
+      `)
 
       broadcast(`${socket.name} joined the chat\n`, socket)
     }
-  })
+  }) 
 
-   // Remove the client from the list when it leaves
-   socket.on('end', function () {
+  socket.on('end', () => {
     clients.splice(clients.indexOf(socket), 1)
-    // broadcast(socket.name + " left the chat.\n")
+    broadcast(socket.name + " left the chat.\n")
+
+    clients.forEach(client => console.log(client.name))
   })
 }).listen(1234)
 
-// Put a friendly message on the terminal of the server.
-console.log("Chat server running at port 1234\n")
+// to use: run `nc 172.46.3.160 1234` OR YOUR IP ADDRESS
